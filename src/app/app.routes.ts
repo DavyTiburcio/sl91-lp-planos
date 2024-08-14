f7bb2d08f1d3b396e33bcab59754c07e48be7ca9
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-08-14 13:29:48 -0300

    fix: test adjust router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 // COMPONENTS
 import { HomeComponent } from './pages/home/home.component';
+import { SucessComponent } from './pages/sucess/sucess.component';
 
 export const routes: Routes = [
   {
@@ -9,6 +10,6 @@ export const routes: Routes = [
   },
   {
     path: 'sucesso',
-    loadComponent: () => import('./pages/sucess/sucess.component').then((p) => p.SucessComponent),
+    component: SucessComponent,
   }
 ];
