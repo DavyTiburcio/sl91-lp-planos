624138ac418779020a16f5e80ecad2369c41a168
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-08-14 13:21:43 -0300

    fix: import component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SucessComponent } from './pages/sucess/sucess.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, SucessComponent],
   template: `
   <router-outlet />
   `,
