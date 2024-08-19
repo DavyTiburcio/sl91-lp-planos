f27dbf29b1ac2b5548b1d7a84c77ed3ebc89ce7e
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-08-19 08:27:16 -0300

    fix: adjust google tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SucessComponent } from './pages/sucess/sucess.component';
+import { filter } from 'rxjs';
 
+declare const gtag: Function;
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,6 +13,27 @@ import { SucessComponent } from './pages/sucess/sucess.component';
   <router-outlet />
   `,
 })
-export class AppComponent {
 
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        // Filtra apenas eventos de término de navegação
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        // Verifica a rota específica
+        if (event.url === '/sucesso') {
+          // Dispara o evento para o Google Analytics
+          gtag('event', 'page_view', {
+            page_path: event.url,
+            page_title: 'Sucesso',
+            page_location: window.location.href,
+          });
+        }
+      });
+  }
 }
