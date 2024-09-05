a025f214cbb6ca9fe8b0e7a3ebb68215effa147b
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-09-05 09:21:36 -0300

    fix: tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SucessComponent } from './pages/sucess/sucess.component';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,23 @@ import { SucessComponent } from './pages/sucess/sucess.component';
 })
 
 export class AppComponent{
-  
+  constructor(
+    private router: Router,
+    private gtmService: GoogleTagManagerService
+  ) {}
+
+  ngOnInit() {
+    // Rastrear mudanças de rota
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      if (event.urlAfterRedirects === '/sucesso') {
+        // Enviar evento de conversão para o GTM
+        this.gtmService.pushTag({
+          event: 'conversion',
+        });
+      }
+    });
+  }
 }
 
